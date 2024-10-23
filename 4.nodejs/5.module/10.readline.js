30cf73baf8796bc992b517f21529a1a591f0847f
cheon1217 <friendly1217@naver.com>
2024-10-23 16:57:59 +0900

    module 공부

--- 4.nodejs/5.module/10.readline.js
+++ 4.nodejs/5.module/10.readline.js
@@ -1,8 +1,9 @@
-var readline = require('linebyline'),
-rl = readline('./somefile.txt');
+var readline = require('linebyline'), rl = readline('./example.txt');
 rl.on('line', function(line, lineCount, byteCount) {
 // do something with the line of text
+console.log(line, lineCount, byteCount);
 })
 .on('error', function(e) {
 // something went wrong
+console.log("어.. 뭔가 오류 발생..", e.message);
 });
