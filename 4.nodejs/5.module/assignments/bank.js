6d0e6b38aafce78990ae53c62f944150ce0dce25
cheon1217 <friendly1217@naver.com>
2024-10-24 17:18:49 +0900

    assignment

--- 4.nodejs/5.module/assignments/bank.js
+++ 4.nodejs/5.module/assignments/bank.js
@@ -4,3 +4,16 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
+
+function bank() {
+    console.log("ATM 메뉴 :");
+    console.log("1. 잔액 확인");
+    console.log("2. 입금");
+    console.log("3. 출금");
+    console.log("4. 종료");
+
+    rl.question("원하는 작업을 선택하세요:", (menu) => {
+        
+    })
+
+}
