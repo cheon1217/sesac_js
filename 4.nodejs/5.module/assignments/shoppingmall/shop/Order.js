f4547ecb0e907f8427bc559a475b26cc3ffde223
cheon1217 <friendly1217@naver.com>
2024-11-02 16:16:18 +0900

    shoppingmall Ex

---
+++ 4.nodejs/5.module/assignments/shoppingmall/shop/Order.js
@@ -0,0 +1,33 @@
+const { Product } = require("./Product");
+
+class Order {
+    constructor(user) {
+        this.user = user;
+        this.products = [];
+        this.totalAmount = 0;
+    }
+
+    addProduct(product, quantity) {
+        if (product.checkAvailability(quantity)) {
+            this.products.push({ product, quantity });
+            this.totalAmount += product.price * quantity;
+            product.updateStock(-quantity);
+        } else {
+            console.log(`Sorry, ${product.name} is out of stock or insufficient quantity`);
+        }
+    }
+
+    getOrderSummary() {
+        return {
+            user: this.user,
+            totalAmount: this.totalAmount,
+            items: this.products.map(({ product, quantity }) => ({
+                name: product.name,
+                quantity: quantity,
+                price: product.price
+            }))
+        };
+    }
+}
+
+module.exports = Order;
---
+++ 4.nodejs/5.module/assignments/shoppingmall/shop/User.js
@@ -0,0 +1,18 @@
+class User {
+    constructor(name, email, address) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.orderHistory = [];
+    }
+
+    addOrder(order) {
+        this.orderHistory.push(order);
+    }
+
+    getOrderHistory() {
+        return this.orderHistory.map(order => order.getOrderSummary());
+    }
+}
+
+module.exports = User;
