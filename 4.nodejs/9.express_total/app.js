38ba713ac4d6667047045dacd2993ab7dc812a45
cheon1217 <friendly1217@naver.com>
2024-11-07 11:28:58 +0900

    express 폴더 구조화 및 주석 추가

--- 4.nodejs/9.express_total/app.js
+++ 4.nodejs/9.express_total/app.js
@@ -4,27 +4,36 @@ const path = require("path");
 const fs = require("fs").promises;
 
 // 변수 정의
+// 서버 설정
 const app = express();
 const PORT = 3000;
+
+// 내부 자료구조
 const users = {};
 
 // 미들웨어
 app.use("/static", express.static("static"));
 app.use("/image", express.static("static/image"));
 
-app.use(express.json());
+app.use(express.json()); // 요청을 바디에 application/json이 있으면?? req.body에 담아줘
+
+app.use((req, res, next) => {
+    console.log(`LOG: ${req.method} ${req.url}`);
+    next();
+});
 
 // 라우트
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "static","index.html"));
 });
 
 app.get("/about", (req, res) => {
-    res.sendFile(path.join(__dirname, "about.html"));
+    res.sendFile(path.join(__dirname, "static", "about.html"));
 });
 
 app.get("/user", (req, res) => {
     res.json(users);
+    // res.send(JSON.stringify(users));
 });
 
 app.post("/user", (req, res) => {
@@ -36,6 +45,7 @@ app.post("/user", (req, res) => {
 
 app.put("/user/:id", (req, res) => { // id 받아오는 것
     try {
+        // const id = req.params.id;
         const { id } = req.params;
         users[id] = req.body.name;
         res.status(200).send("수정 성공");
@@ -47,9 +57,11 @@ app.put("/user/:id", (req, res) => { // id 받아오는 것
 
 app.delete("/user/:id", (req, res) => { // id 받아오는 것
     try {
+        // const id = req.params.id;
         const { id } = req.params;
         delete users[id];
-        res.status(204).send("삭제 성공");
+        res.status(204).send();
+        // res.status(200).send("삭제 완료");
     } catch (err) {
         console.error("삭제 요청 중 에러 발생:", err);
         res.status(500).send("서버 오류");
@@ -65,7 +77,7 @@ app.use(async (req, res, next) => {
         console.error("404 page fail", err);
         res.status(500).send("서버 오류");
     }
-})
+});
 
 // 서버 시작
 app.listen(PORT, () => {
