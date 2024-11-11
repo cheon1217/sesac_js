749f64cb28fdb8c3bec912604aba9cb8dbdb64ef
cheon1217 <friendly1217@naver.com>
2024-11-11 15:26:04 +0900

    common function

---
+++ 4.nodejs/12.session/5.cart_restapi/public/js/checkuser.js
@@ -0,0 +1,44 @@
+ export function fetch_checkLoginStatus() {
+    return fetch("/api/check-login")
+        .then((response) => response.json())
+        .then((userData) => {
+            if (userData.username) {
+                document.getElementById("user-info").innerHTML = `
+                    ${userData.username}님
+                    <span class="logout-btn" id="logout">Logout</span>
+                `
+                document.getElementById("user-info").style.display = "block";
+
+                document.getElementById("logout").addEventListener("click", logout);
+
+                // showProfile(userData.username);
+                return userData.username;
+            } else {
+                document.getElementById("user-info").style.display = "none";
+                return null;
+            }
+        })
+        .catch((err) => {
+            console.error("Login Check Error: ", err.message);
+            return null;
+        })
+}
+
+function logout() {
+    fetch("/api/logout")
+        .then((response) => {
+            if (response.ok) {
+                // 로그아웃 성공 시 홈으로 돌려보내기
+                // window.location.href = "/";
+                return response.json();
+            } else {
+                // 로그아웃 실패
+                
+            }
+        })
+        .then((data) => {
+            if (data.redirectUrl) { // 이 케이스는 벡엔드에서 로그아웃 이후 갈 곳을 보내주는 경우
+                window.location.href = data.redirectUrl;
+            }
+        })
+}
