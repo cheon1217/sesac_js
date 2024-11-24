2d96f59b768f72ec19691336db589718fddb437c
cheon1217 <friendly1217@naver.com>
2024-11-25 01:07:03 +0900

    CRM items part

---
+++ 6.crm/CRM/public/js/items.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetchItem(1);
+});
+
+async function fetchItem(page) {
+    const response = await fetch(`/api/items?page=${page}`);
+
+    const data = await response.json();
+
+    const tableHeader = document.getElementById("table-header");
+    const tableBody = document.getElementById("table-body");
+
+    tableHeader.innerHTML = "";
+    tableBody.innerHTML = "";
+
+    const headerRow = document.createElement("tr");
+    const fields = Object.keys(data.result[0]);
+    fields.forEach((field) => {
+            const th = document.createElement("th");
+            th.textContent = field;
+            headerRow.appendChild(th);
+    });
+    tableHeader.appendChild(headerRow);
+
+    data.result.forEach((row) => {
+        const bodyRow = document.createElement("tr");
+
+        for (const [key, value] of Object.entries(row)) {
+            const td = document.createElement("td");
+            if (key === "id") {
+                td.innerHTML = `<a href="/items/${value}">${value}</a>`;
+            } else {
+                td.textContent = value;
+            }
+            bodyRow.appendChild(td);
+        }
+        tableBody.appendChild(bodyRow);
+    });
+    displayPagination(parseInt(data.currentPage), parseInt(data.totalPage));
+}
+
+function displayPagination(currentPage, totalPage) {
+    const pagination = document.getElementById("pagination");
+    pagination.innerHTML = "";
+
+    const prevButton = document.createElement("button");
+    prevButton.textContent = "이전";
+    if (currentPage > 1) {
+        prevButton.onclick = () => fetchItem(currentPage - 1);
+    }
+    pagination.appendChild(prevButton);
+
+    const pageInfo = document.createElement("span");
+    pageInfo.textContent = `${currentPage} / ${totalPage}`;
+    pagination.appendChild(pageInfo);
+
+    const nextButton = document.createElement("button");
+    nextButton.textContent = "다음";
+    if (currentPage < totalPage) {
+        nextButton.onclick = () => fetchItem(currentPage + 1);
+    }
+    pagination.appendChild(nextButton);
+}
