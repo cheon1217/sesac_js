cdd348ceb937f4e6ed8e2b86ff554518a28e98f0
cheon1217 <friendly1217@naver.com>
2024-11-25 17:45:02 +0900

    db를 이용한 chartjs 연습

---
+++ 7.canvas/crm/app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const sqlite3 = require("sqlite3");
+const nunjucks = require("nunjucks");
+
+const app = express();
+const port = 3000;
+
+// 눈적스 초기화
+nunjucks.configure("views", {
+    autoescape: true,
+    express: app,
+});
+
+app.set("view engine", "html"); // 눈적스 기본 확장자 njk -> html로 변경
+
+app.get("/", (req, res) => {
+    const db = new sqlite3.Database("user-sample.db", (err) => {
+        if (err) {
+            console.log("파일없다");
+        } else {
+            console.log("DB 로딩 성공");
+        }
+    });
+    
+    db.all(`
+       SELECT 
+            strftime('%Y-%m', "orders"."OrderAt") AS YearMonth,
+            SUM(items.UnitPrice) AS MonthlyRevenue
+        FROM 
+            "orders"
+        JOIN 
+            "order_items" ON "orders"."Id" = "order_items"."OrderId"
+        JOIN 
+            "items" ON "order_items"."ItemId" = "items"."Id"
+        WHERE 
+            "orders"."OrderAt" >= date('now', '-2 year')
+        GROUP BY 
+            strftime('%Y-%m', "orders"."OrderAt")
+        ORDER BY 
+            strftime('%Y-%m', "orders"."OrderAt")
+    `, [], (err, rows) => {
+        if (err) {
+            console.error("실패!");
+        } else {
+            // console.log(rows);
+            // res.render("monthly_revenue", { rows });
+            const labels = rows.map((row) => row.YearMonth);
+            const revenues = rows.map((row) => row.MonthlyRevenue);
+
+            res.render("monthly_revenue", {
+                rows: rows,
+                labels: JSON.stringify(labels),
+                revenues: JSON.stringify(revenues),
+            });
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log("Server Ready");
+});
