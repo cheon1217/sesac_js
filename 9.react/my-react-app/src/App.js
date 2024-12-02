511a6754130cc90e1a176d4810ec8f5cb7ab6fd9
cheon1217 <friendly1217@naver.com>
2024-12-02 14:53:56 +0900

    react my-app

---
+++ 9.react/my-react-app/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          수정 <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Learn React?
+        </p>
+        <p>
+          <a href='https://sesac.seoul.kr/'>새싹에서 배우는 리엑트</a>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ 9.react/my-react-app/src/App2.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>여긴 app2</h1>
+        <p>
+          수정 <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Learn React?
+        </p>
+        <p>
+          <a href='https://sesac.seoul.kr/'>새싹에서 배우는 리엑트</a>
+        </p>
+        <p>우앙 신기하당</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
