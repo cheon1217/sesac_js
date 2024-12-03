627a32e7c42127fd1359a744e8aa6828601aa127
cheon1217 <friendly1217@naver.com>
2024-12-03 13:22:12 +0900

    react study

--- 9.react/1.my-app/src/index.js
+++ 9.react/1.my-app/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const App = () => {
-  return <h1>헬로우 리엑트!</h1>
-}
+// function App() {
+//   return <h1>Hello, World!</h1>;
+// }
 
-const Banner = () => {
-  return <h1>여기는 광고가 올 곳!</h1>
-}
+// const App = () => {
+//   return <h1>Hello, World!</h1>;
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Banner />
   </React.StrictMode>
 );
