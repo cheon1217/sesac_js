eeeaf83931d7fba3dd91ee4050dfc920b1249e6f
cheon1217 <friendly1217@naver.com>
2024-12-04 16:37:01 +0900

    react bootstrap design

---
+++ 9.react/8.design/src/App.js
@@ -0,0 +1,26 @@
+import { Container, Row, Col, Button, Badge, Alert } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+    return (
+        <Container className="mt-5">
+            <Row>
+                <Col>
+                    <Alert variant="info" className="text-center">
+                        <h1>React with Bootstrap</h1>
+                    </Alert>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Badge bg="primary">Primary</Badge>
+                    <Badge bg="secondary">Secondary</Badge>
+                    <Badge bg="success">Success</Badge>
+                    <Badge bg="danger">Danger</Badge>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default App;
